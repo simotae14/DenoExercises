03b506d89f39bd372c0b7c822808439347ae4205
Simone <startae14@gmail.com>
2021-04-21 00:16:37 +0200

    12. grant permissions in the code of the file without flags

--- 11-grant-permissions-in-deno/app.ts
+++ 11-grant-permissions-in-deno/app.ts
@@ -1,7 +1,20 @@
-const results = await Deno.readDir("../");
+const desc = {
+  name: "read",
+  path: "../"
+} as const;
 
-for await (const result of results) {
-  console.log(result);
+const status = await Deno.permissions.request(desc);
+
+if (status.state === "granted") {
+  const results = await Deno.readDir("../");
+
+  for await (const result of results) {
+    console.log(result);
+  }
+} else {
+  console.log("This program need permissions to read the current directory only.");
 }
+
+
 
 export {};
