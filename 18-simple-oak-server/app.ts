048559cdf1333615c6ef43eb6e8e1def7b44fe51
Simone <startae14@gmail.com>
2021-04-22 23:28:32 +0200

    20: a simple oak server

---
+++ 18-simple-oak-server/app.ts
@@ -0,0 +1,20 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const PORT = 3001;
+const HOSTNAME = "0.0.0.0";
+
+// create an Application instance
+const app = new Application();
+
+// handler for the request
+app.use((ctx) => {
+  ctx.response.body = "Hello World Oak!"
+});
+
+console.log(`Now listening on http://${HOSTNAME}:${PORT}`);
+
+// listen
+app.listen({
+  port: PORT,
+  hostname: HOSTNAME
+});
