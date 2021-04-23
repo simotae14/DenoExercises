b1443ec54fc2e9613d509b93c9cec46655699880
Simone <startae14@gmail.com>
2021-04-23 21:17:34 +0200

    31: starter files to dockerize the app

---
+++ 24-docker-containerize-the-app/deps.ts
@@ -0,0 +1,5 @@
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+// fetch handlebars dependency
+import { Handlebars } from "https://deno.land/x/handlebars/mod.ts";
+
+export { Application, Router, Handlebars, send };
